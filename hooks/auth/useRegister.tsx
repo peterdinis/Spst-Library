5e13d1da43bd36719ee8dea1e88ef1dcc3aea9e1
peterdinis <petter1@pobox.sk>
2025-09-13 18:57:41 +0200

    feat: Create new auth hooks

---
+++ hooks/auth/useRegister.tsx
@@ -0,0 +1,22 @@
+import { RegisterDto, Tokens } from "@/types/authTypes"
+import { useMutation } from "@tanstack/react-query"
+
+export const useRegister = () => {
+    return useMutation<Tokens, Error, RegisterDto>({
+        mutationKey: ["registerUser"],
+        mutationFn: async (dto) => {
+            const res = await fetch('/auth/register', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(dto),
+            });
+
+            if (!res.ok) {
+                const err = await res.json();
+                throw new Error(err.message || 'Registration failed');
+            }
+
+            return res.json();
+        },
+    })
+}
