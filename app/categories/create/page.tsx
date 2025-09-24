c046bbe15fe3d852dc94660a06ac741b6c8db64b
peterdinis <petter1@pobox.sk>
2025-09-24 16:59:08 +0200

    feat: Start to cleaning

--- app/categories/create/page.tsx
+++
@@ -1,8 +0,0 @@
-import CreateCategoryForm from "@/components/categories/CreateCategoryForm";
-import { NextPage } from "next";
-
-const CreateCategoryPage: NextPage = () => {
-  return <CreateCategoryForm />;
-};
-
-export default CreateCategoryPage;
