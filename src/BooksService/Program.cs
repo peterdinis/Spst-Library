5b170b6c623f4843c5a49c28fc06c8e1adcbd842
peterdinis <petter1@pobox.sk>
2025-11-15 21:15:45 +0100

    feat: Working on booksService WIP

--- src/BooksService/Program.cs
+++ src/BooksService/Program.cs
@@ -1,6 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using BooksService.Data;
+using BooksService.Validators;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 builder.Services.AddControllers();
+
+builder.Services.AddFluentValidationAutoValidation();
+builder.Services.AddValidatorsFromAssemblyContaining<BookValidator>();
 
 var app = builder.Build();
 
