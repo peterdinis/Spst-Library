5ba5b7afbd979b7aa8acde02ef25764b1ea9a30d
peterdinis <petter1@pobox.sk>
2025-11-16 13:38:30 +0100

    feat: Add migration for AuthorService

--- src/AuthorService/obj/Debug/net9.0/AuthorService.AssemblyInfo.cs
+++ src/AuthorService/obj/Debug/net9.0/AuthorService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthorService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a45a67f6fb2ec63f5c60b8fa5c21de8b60c48e58")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8efc6bae393a80c319c51c1bc6327acc1ba93b5f")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthorService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthorService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
