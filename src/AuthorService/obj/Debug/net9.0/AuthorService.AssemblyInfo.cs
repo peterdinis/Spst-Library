dadcdf0d078d91b99aea3b099659ff0f988f3084
peterdinis <petter1@pobox.sk>
2025-11-16 14:45:58 +0100

    feat: Working on publisher service WIP

--- src/AuthorService/obj/Debug/net9.0/AuthorService.AssemblyInfo.cs
+++ src/AuthorService/obj/Debug/net9.0/AuthorService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthorService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5c1c9eece6c5a9708e8461dd6a0b6003f567c8d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cf63afa031238b5b0d085245c0da2714c5eb6d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthorService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthorService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
