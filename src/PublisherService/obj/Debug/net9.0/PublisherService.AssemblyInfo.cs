43793990b06232ae452775f9b81bb0678df56d50
peterdinis <petter1@pobox.sk>
2025-11-16 18:43:52 +0100

    feat: Publisher microservice is DONE

--- src/PublisherService/obj/Debug/net9.0/PublisherService.AssemblyInfo.cs
+++ src/PublisherService/obj/Debug/net9.0/PublisherService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PublisherService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc20b17e349227b0d3826263b0138eaa00580ddb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63738478998672f75a2d663649da160d26a4ef86")]
 [assembly: System.Reflection.AssemblyProductAttribute("PublisherService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PublisherService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
