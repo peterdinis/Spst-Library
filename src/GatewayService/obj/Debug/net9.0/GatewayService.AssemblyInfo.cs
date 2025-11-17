0710456c48a4e784fc5a4eacf403e1a40aec4a23
peterdinis <petter1@pobox.sk>
2025-11-17 09:55:22 +0100

    feat: Merge master

--- src/GatewayService/obj/Debug/net9.0/GatewayService.AssemblyInfo.cs
+++ src/GatewayService/obj/Debug/net9.0/GatewayService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GatewayService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+546560a53408befe7e65218e8121dac39879cd0c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3cc922ce69c011bcea42757a9b083c06ea4bf7b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("GatewayService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GatewayService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
