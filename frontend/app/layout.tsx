8056f53f6653d8d9fa45cc6647cb2ba2e0d056ad
peterdinis <petter1@pobox.sk>
2025-11-22 21:08:24 +0100

    feat: Removing fluent ui

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -28,9 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <AppFluentProvider>
           {children}
-        </AppFluentProvider>
       </body>
     </html>
   );
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,16 +1,9 @@
-import HomeWrapper from '@/components/home/HomeWrapper';
-import LibraryNavigation from '@/components/shared/navigation/Navigation';
 import { NextPage } from 'next';
 
 const App: NextPage = () => {
   return (
     <div>
-      <LibraryNavigation
-        userRole="student"
-        userName="John Doe"
-      />
-
-      <HomeWrapper />
+      HOME
     </div>
   );
 };
