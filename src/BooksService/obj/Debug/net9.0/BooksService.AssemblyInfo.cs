5b852f73fbe7d54278bb4a93fae67aeed14fda29
peterdinis <petter1@pobox.sk>
2025-11-22 19:02:50 +0100

    feat: Continue working on relations WIP

--- src/BooksService/obj/Debug/net9.0/BooksService.AssemblyInfo.cs
+++ src/BooksService/obj/Debug/net9.0/BooksService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BooksService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4d0c25f5e67aa5efecce711e5b4dee9225ab452")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a3ef0406a01166815a2e60edd058be09d59deba")]
 [assembly: System.Reflection.AssemblyProductAttribute("BooksService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BooksService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
