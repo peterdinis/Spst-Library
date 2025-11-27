d772641f0a4b97b0d84bf58b080d2394c5c35be0
peterdinis <petter1@pobox.sk>
2025-11-27 18:30:22 +0100

    Remove publisher microservice

--- src/PublisherService/Data/ApplicationDbContext.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.EntityFrameworkCore;
-using PublisherService.Entities;
-
-namespace PublisherService.Data;
-
-public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
-{
-    public DbSet<Publisher> Publishers { get; set; }
-}
