4c39eb9847091f9c36c4c03aabec4cf8ac41b99d
peterdinis <petter1@pobox.sk>
2025-11-29 16:24:25 +0100

    feat: Finish details pages WIP

--- frontend/app/categories/[...id]/page.tsx
+++ frontend/app/categories/[...id]/page.tsx
@@ -1,8 +1,18 @@
+import { CategoryDetail } from "@/components/categories/CategoryDetail";
 import { NextPage } from "next";
 
 const CategoryDetailPage: NextPage = () => {
     return (
-        <>ABCD</>
+       <CategoryDetail
+          name="Svetová literatúra"
+          description="Objavte najvýznamnejšie diela svetovej literatúry od klasických autorov až po súčasných spisovateľov. Táto kategória obsahuje romány, poviedky a eseje, ktoré formovali literatúru a ovplyvnili generácie čitateľov."
+          icon="📚"
+          totalBooks={156}
+          availableBooks={89}
+          popularBooks={24}
+          newBooks={12}
+          mostBorrowedGenre="Klasická próza"
+        />
     )
 }
 
