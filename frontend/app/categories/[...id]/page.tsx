6a91017d958159819aa078f04ea0f633dfa88ccc
peterdinis <petter1@pobox.sk>
2025-11-29 16:35:12 +0100

    feat: Formatting files

--- frontend/app/categories/[...id]/page.tsx
+++ frontend/app/categories/[...id]/page.tsx
@@ -3,15 +3,15 @@ import { NextPage } from "next";
 
 const CategoryDetailPage: NextPage = () => {
     return (
-       <CategoryDetail
-          name="Svetová literatúra"
-          description="Objavte najvýznamnejšie diela svetovej literatúry od klasických autorov až po súčasných spisovateľov. Táto kategória obsahuje romány, poviedky a eseje, ktoré formovali literatúru a ovplyvnili generácie čitateľov."
-          icon="📚"
-          totalBooks={156}
-          availableBooks={89}
-          popularBooks={24}
-          newBooks={12}
-          mostBorrowedGenre="Klasická próza"
+        <CategoryDetail
+            name="Svetová literatúra"
+            description="Objavte najvýznamnejšie diela svetovej literatúry od klasických autorov až po súčasných spisovateľov. Táto kategória obsahuje romány, poviedky a eseje, ktoré formovali literatúru a ovplyvnili generácie čitateľov."
+            icon="📚"
+            totalBooks={156}
+            availableBooks={89}
+            popularBooks={24}
+            newBooks={12}
+            mostBorrowedGenre="Klasická próza"
         />
     )
 }
