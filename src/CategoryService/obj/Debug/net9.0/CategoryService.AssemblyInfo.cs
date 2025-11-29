24c3197dc6c3a6ce22e8ae65b165fbb173efe141
peterdinis <petter1@pobox.sk>
2025-11-29 09:29:53 +0100

    FOO

--- src/CategoryService/obj/Debug/net9.0/CategoryService.AssemblyInfo.cs
+++ src/CategoryService/obj/Debug/net9.0/CategoryService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CategoryService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbc5eaf330af6407494a3236e9de791deb08912c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+686625b71cdbb2e343232f40d7d5ee6e715a7740")]
 [assembly: System.Reflection.AssemblyProductAttribute("CategoryService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CategoryService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
