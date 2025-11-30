d1cb736508a1a6ebb69721081431a2b0881c141c
peterdinis <petter1@pobox.sk>
2025-11-30 14:40:14 +0100

    FOO

--- src/IdentityService/obj/Debug/net9.0/IdentityService.AssemblyInfo.cs
+++ src/IdentityService/obj/Debug/net9.0/IdentityService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IdentityService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cbb08be48ed5b49874a841cc3f58cc14262c3e6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24ad2ec036c9132d722bb54c83198aa278b546f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("IdentityService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IdentityService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
