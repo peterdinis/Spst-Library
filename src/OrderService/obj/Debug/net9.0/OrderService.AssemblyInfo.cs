6329f5c4da2c75a4a604036142289dc26ddcf36b
peterdinis <petter1@pobox.sk>
2025-12-06 17:25:29 +0100

    feat: Migrations in order service

--- src/OrderService/obj/Debug/net9.0/OrderService.AssemblyInfo.cs
+++ src/OrderService/obj/Debug/net9.0/OrderService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OrderService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35dfd3cb37e7ec813da65d58eb3674e193d1aa26")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75fa952e2027ef430007018ae2cb933b02af6eb4")]
 [assembly: System.Reflection.AssemblyProductAttribute("OrderService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OrderService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
