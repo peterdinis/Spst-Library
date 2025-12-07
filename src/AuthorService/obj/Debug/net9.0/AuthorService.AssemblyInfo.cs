4daf0a139b69dc1eda859e369074f321fb462017
peterdinis <petter1@pobox.sk>
2025-12-07 10:20:43 +0100

    FOO

--- src/AuthorService/obj/Debug/net9.0/AuthorService.AssemblyInfo.cs
+++ src/AuthorService/obj/Debug/net9.0/AuthorService.AssemblyInfo.cs
@@ -13,7 +13,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthorService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+<<<<<<< Updated upstream
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec95402c530862fee2e8f9c6f9aa8d93b2c787f7")]
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da814ff2ae7482735d96d0196284864abcc1e1fd")]
+>>>>>>> Stashed changes
 [assembly: System.Reflection.AssemblyProductAttribute("AuthorService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthorService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
