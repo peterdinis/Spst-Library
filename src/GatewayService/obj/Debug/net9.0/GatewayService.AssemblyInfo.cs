bfd7e840d72a296551e19f9c96965c132231ec84
peterdinis <petter1@pobox.sk>
2025-12-07 11:20:17 +0100

    refactor: Move hooks from lib folder

--- src/GatewayService/obj/Debug/net9.0/GatewayService.AssemblyInfo.cs
+++ src/GatewayService/obj/Debug/net9.0/GatewayService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GatewayService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cfd223d51e245b430f2cb64b24039a004a24708")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f18b65a3e4ed2b1ee67f5168f59f4bdf75fc21f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("GatewayService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GatewayService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
