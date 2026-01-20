85756840c94cb9f14fbe1cddae12a0529e3ede0e
peterdinis <petter1@pobox.sk>
2026-01-20 18:46:26 +0100

    fix: Start to working on the bug

---
+++ src/routes/categories.$categoryId.tsx
@@ -0,0 +1,9 @@
+import { createFileRoute } from '@tanstack/react-router'
+
+export const Route = createFileRoute('/categories/$categoryId')({
+  component: RouteComponent,
+})
+
+function RouteComponent() {
+  return <div>Hello "/categories/$categoryId"!</div>
+}
