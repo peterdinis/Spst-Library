d6474ea4671817eb807da12773558c1c454ac690
peterdinis <petter1@pobox.sk>
2026-01-22 18:42:02 +0100

    infra: Fixing issue with uploadthing

--- src/server/uploadthing.ts
+++ src/server/uploadthing.ts
@@ -1,42 +1,33 @@
-import { createUploadthing, UploadThingError } from "uploadthing/server";
-import type { FileRouter } from "uploadthing/server";
+import { createUploadthing, type FileRouter } from "uploadthing/server";
 
 const f = createUploadthing();
 
-const auth = (req: Request) => ({ id: "fakeId" }); // Fake auth function
-
-// FileRouter for your app, can contain multiple FileRoutes
+// FileRouter for author image uploads
 export const uploadRouter = {
-	// Define as many FileRoutes as you like, each with a unique routeSlug
-	imageUploader: f({
+	authorImage: f({
 		image: {
-			/**
-			 * For full list of options and defaults, see the File Route API reference
-			 * @see https://docs.uploadthing.com/file-routes#route-config
-			 */
 			maxFileSize: "4MB",
 			maxFileCount: 1,
 		},
 	})
-		// Set permissions and file types for this FileRoute
 		.middleware(async ({ req }) => {
-			// This code runs on your server before upload
-			const user = await auth(req);
-
-			// If you throw, the user will not be able to upload
-			if (!user) throw new UploadThingError("Unauthorized");
-
-			// Whatever is returned here is accessible in onUploadComplete as `metadata`
-			return { userId: user.id };
+			// TODO: Add real authentication when available
+			// For now, we'll allow uploads without strict auth
+			return { userId: "anonymous" };
 		})
 		.onUploadComplete(async ({ metadata, file }) => {
-			// This code RUNS ON YOUR SERVER after upload
 			console.log("Upload complete for userId:", metadata.userId);
+			console.log("File uploaded:", file);
 
-			console.log("file url", file.ufsUrl);
-
-			// !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
-			return { uploadedBy: metadata.userId };
+			// Return all necessary data for Convex integration
+			return {
+				uploadedBy: metadata.userId,
+				fileKey: file.key,
+				fileName: file.name,
+				fileUrl: file.url,
+				fileSize: file.size,
+				fileType: file.type,
+			};
 		}),
 } satisfies FileRouter;
 
