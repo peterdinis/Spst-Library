1a4b8faa35b79bdb655c0ee7986667820100a49e
Peter Dinis <petter1@pobx.sk>
2026-05-02 13:07:47 +0200

    feat: Updating route for uploadthing

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -9,7 +9,7 @@ const f = createUploadthing();
 
 export const ourFileRouter = {
 	bookCover: f({
-		image: { maxFileSize: "4MB", maxFileCount: 1 },
+		image: { maxFileSize: "8MB", maxFileCount: 1 },
 	})
 		.middleware(async () => {
 			const session = await auth();
@@ -34,7 +34,7 @@ export const ourFileRouter = {
 		}),
 
 	authorPhoto: f({
-		image: { maxFileSize: "4MB", maxFileCount: 1 },
+		image: { maxFileSize: "8MB", maxFileCount: 1 },
 	})
 		.middleware(async () => {
 			const session = await auth();
